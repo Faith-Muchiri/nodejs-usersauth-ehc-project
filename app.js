faf8048407cf8c7a1dc52006280cf7006e2d5abe
Faith <faithwambui891@gmail.com>
2023-05-08 03:33:52 +0300

    add validation files

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDb = require('./config/dbConnection');
+const errorHandler = require('./middleware/errorHandler');
+const dotnev = require('dotenv').config();
+
+connectDb();
+const app = express();
+
+// create port
+const port = process.env.PORT || 8000;
+
+app.use(express.json());
+app.use("/api/users", require("./routes/userRoutes"));
+app.use(errorHandler);
+
+// starting a server
+app.listen(port, () => {
+    console.log(`App is running at ${port}`)
+})
