660dbb44182dcd6ee4c0d66ff3ef604ad74ac933
Faith <faithwambui891@gmail.com>
2023-05-11 16:02:14 +0300

    configure google auth

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,10 @@ const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+const { v4: uuidv4 } = require('uuid');
+
 
 // @desc Register a user~~
 // @route POST /api/users/register
@@ -18,6 +22,9 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("User already registered");
   }
 
+  // Generate Google ID for the user
+  const googleId = `google_${uuidv4()}`;
+
   // Hash password
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log("Hashed password", hashedPassword);
@@ -25,6 +32,7 @@ const registerUser = asyncHandler(async (req, res) => {
     username,
     email,
     password: hashedPassword,
+    googleId,
   });
 
   console.log(`user created ${user}`);
@@ -34,72 +42,68 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("User data is not valid");
   }
-  res.json({ message: "Register the user" });
 });
 
 //@desc Login user
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(400);
-    throw new Error("All fields are mandatory!");
-  }
-  const user = await User.findOne({ email });
-  //compare password with hashedpassword
-  if (user && (await bcrypt.compare(password, user.password))) {
-    const accessToken = jwt.sign(
-      {
-        user: {
-          username: user.username,
-          email: user.email,
-          id: user.id,
-        },
-      },
-      process.env.ACCESS_TOKEN_SECRET,
-      { expiresIn: "15m" }
-    );
-    // generate new OTP and store in database
-    const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    user.otp = otp;
-    await user.save();
-    console.log(`Generated OTP ${otp}`);
-    // send OTP to user's email (implementation not shown)
-    res.status(200).json({ accessToken });
-  } else {
-    res.status(401);
-    throw new Error("email or password is not valid");
-  }
-});
+  const { email, password, googleIdToken } = req.body;
 
-// const loginUser = asyncHandler(async (req, res) => {
-//   const { email, password } = req.body;
-//   if (!email || !password) {
-//     res.status(400);
-//     throw new Error("All fields are mandatory!");
-//   }
-//   const user = await User.findOne({ email });
-//   //compare password with hashedpassword
-//   if (user && (await bcrypt.compare(password, user.password))) {
-//     const accessToken = jwt.sign(
-//       {
-//         user: {
-//           username: user.username,
-//           email: user.email,
-//           id: user.id,
-//         },
-//       },
-//       process.env.ACCESS_TOKEN_SECRET,
-//       { expiresIn: "15m" }
-//     );
-//     console.log(accessToken)
-//     res.status(200).json({ accessToken });
-//   } else {
-//     res.status(401);
-//     throw new Error("email or password is not valid");
-//   }
-// });
+  if ((!email || !password) && !googleIdToken) {
+    res.status(400);
+    throw new Error('All fields are mandatory!');
+  }
+
+  let user;
+  let accessToken;
+  let otp;
+
+  if (googleIdToken) {
+    // Authenticate with Google SSO
+    const ticket = await client.verifyIdToken({
+      idToken: googleIdToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const { email: googleEmail } = ticket.getPayload();
+
+    user = await User.findOneAndUpdate(
+      { email: googleEmail },
+      { email: googleEmail },
+      { new: true, upsert: true },
+    );
+  } else {
+    // Authenticate with email and password
+    const existingUser = await User.findOne({ email });
+    if (existingUser && (await bcrypt.compare(password, existingUser.password))) {
+      user = existingUser;
+    } else {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+  }
+
+  // Generate and save an OTP to the user's document
+  otp = Math.floor(100000 + Math.random() * 900000).toString();
+  user.otp = otp;
+  await user.save();
+  console.log(`Generated OTP ${otp}`);
+
+  // Generate a new access token and return it to the client
+  accessToken = jwt.sign(
+    {
+      user: {
+        username: user.username,
+        email: user.email,
+        id: user.id,
+      },
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: '15m' },
+  );
+  res.status(200).json({ accessToken, otp });
+});
 
 //@desc Current user info
 //@route POST /api/users/current
@@ -114,9 +118,9 @@ const currentUser = asyncHandler(async (req, res) => {
 const logoutUser = asyncHandler(async (req, res) => {
   const { accessToken } = req.body;
 
-  // Remove the access token from the database
-  await User.findOneAndUpdate(
-    { _id: req.user.id },
+  // Remove the access token from the user's document in the database
+  await User.findByIdAndUpdate(
+    req.user.id,
     { accessToken: null }
   );
 
